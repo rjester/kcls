1d862b47f7891fbb96e4ae97b670dde0013e9c02
Russ Jester <rjester@gmail.com>
2014-07-20 10:55:42 -0400

    initial commit

---
+++ js/default.js
@@ -0,0 +1,15 @@
+﻿// Copyright (C) Microsoft Corporation. All rights reserved.
+(function () {
+    "use strict";
+
+    // Start executing after the DOM is loaded.
+    document.onreadystatechange = function () {
+        if (document.readyState === "complete") {
+            document.getElementById("link").onclick = showAlert;
+        }
+    };
+
+    function showAlert() {
+        alert("Welcome to Pure HTML!");
+    };
+}());
